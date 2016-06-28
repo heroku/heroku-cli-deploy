4d5da86c2448e25b7fbf1356205b22130fbc17bf
Joe Kutner <jpkutner@gmail.com>
2016-06-27 23:32:26 -0500

    Implemented jar command and factored out common helpers

--- commands/jar.js
+++ commands/jar.js
@@ -2,20 +2,65 @@
 
 const cli = require('heroku-cli-util');
 const co = require('co');
-const child = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const helpers = require('../lib/helpers')
 
 module.exports = function(topic) {
   return {
     topic: topic,
     command: 'jar',
-    flags: [{ name: 'file' }],
-    description: 'Deploys a JAR file to Heroku.',
-    needsApp: false,
+    flags: [
+        { name: 'jar', char: 'j', hasValue: true },
+        { name: 'jdk', hasValue: true },
+        { name: 'includes', char: 'i', hasValue: true },
+        { name: 'options', char: 'o', hasValue: true}],
+    variableArgs: true,
+    description: 'Deploys an executable JAR file to Heroku.',
+    needsApp: true,
     needsAuth: true,
     run: cli.command(co.wrap(jar))
   };
 };
 
 function* jar(context, heroku) {
-  cli.error("Error: Not implemented");
+  var file = getWarFilename(context)
+
+  validate(!file.endsWith('.war') && !file.endsWith('.jar'),
+           'JAR file must have a .jar or .war extension')
+
+  validate(!fs.existsSync(file),
+           'JAR file not found: ' + file)
+
+  validate(fs.statSync(file).size > helpers.maxFileSize(),
+           `JAR file must not exceed ${helpers.maxFileSize()} MB`)
+
+  let status = yield deployWar(warFile, context)
+}
+
+function deployJar(file, context) {
+  console.log('Uploading ' + path.basename(file))
+  return helpers.deploy(context, [
+    `-Dheroku.jarFile=${file}`,
+    `-Dheroku.jarOpts=${context.flags.options.gsub('$','\$')}`,
+    `-cp`, helpers.herokuDeployJar()
+  ]);
+}
+
+function getJarFilename(context) {
+  if (context.args.length > 0) {
+    return path.join(process.cwd(), context.args[0])
+  } else if (context.flags.jar) {
+    return path.join(process.cwd(), context.flags.jar)
+  } else {
+    cli.error("No .jar specified.\nSpecify which war to use with --jar <jar file name>");
+    process.exit(1)
+  }
+}
+
+function validate(condition, message) {
+  if (condition) {
+    cli.error(message)
+    process.exit(1)
+  }
 }
--- commands/war.js
+++ commands/war.js
@@ -3,13 +3,18 @@
 const cli = require('heroku-cli-util');
 const co = require('co');
 const path = require('path');
+const fs = require('fs');
 const helpers = require('../lib/helpers')
 
 module.exports = function(topic) {
   return {
     topic: topic,
     command: 'war',
-    flags: [{ name: 'war', char: 'w' }],
+    flags: [
+        { name: 'war', char: 'w', hasValue: true },
+        { name: 'jdk', char: 'j', hasValue: true },
+        { name: 'includes', char: 'i', hasValue: true },
+        { name: 'webapp-runner', hasValue: true}],
     variableArgs: true,
     description: 'Deploys a WAR file to Heroku.',
     needsApp: true,
@@ -19,20 +24,43 @@ module.exports = function(topic) {
 };
 
 function* war(context, heroku) {
-  var warFile = context.args.length > 0 ?
-      path.join(process.cwd(), context.args[0]) :
-      path.join(process.cwd(), context.flags.war)
+  var warFile = getWarFilename(context)
 
-  // TODO validate the file
+  validate(!warFile.endsWith('.war'),
+           'War file must have a .war extension')
 
-  let status = yield deployWar(warFile, context.app)
+  validate(!fs.existsSync(warFile),
+           'War file not found: ' + warFile)
+
+  validate(fs.statSync(warFile).size > helpers.maxFileSize(),
+           `War file must not exceed ${helpers.maxFileSize()} MB`)
+
+  let status = yield deployWar(warFile, context)
 }
 
-function deployWar(warFile, appName) {
-  let args = [
-    `-Dheroku.warFile=${warFile}`,
-  ];
+function deployWar(file, context) {
+  console.log('Uploading ' + path.basename(file))
+  return helpers.deploy(context, [
+    `-Dheroku.warFile=${file}`,
+    '-jar',
+    helpers.herokuDeployJar()
+  ]);
+}
 
-  console.log('Uploading ' + path.basename(warFile))
-  return helpers.deploy(args, appName);
+function getWarFilename(context) {
+  if (context.args.length > 0) {
+    return path.join(process.cwd(), context.args[0])
+  } else if (context.flags.war) {
+    return path.join(process.cwd(), context.flags.war)
+  } else {
+    cli.error("No .war specified.\nSpecify which war to use with --war <war file name>");
+    process.exit(1)
+  }
+}
+
+function validate(condition, message) {
+  if (condition) {
+    cli.error(message)
+    process.exit(1)
+  }
 }
